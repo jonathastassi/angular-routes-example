4f02f6e1c430e963e4f31a54ed29e94e96be9d2f
Jonathas Tassi e Silva <jonathastassi@hotmail.com>
2019-11-18 23:09:38 -0300

    Deixando o módulo principal enxuto para ser feito o lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BookComponent } from './pages/book/book.component';
-import { DvdComponent } from './pages/dvd/dvd.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
-import { DvdDetailComponent } from './pages/dvd/dvd-detail/dvd-detail.component';
-import { DvdFormComponent } from './pages/dvd/dvd-form/dvd-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BookDetailComponent } from './pages/book/book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent,
-    DvdComponent,
     PageNotFoundComponent,
-    DvdDetailComponent,
-    DvdFormComponent,
-    BookDetailComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
